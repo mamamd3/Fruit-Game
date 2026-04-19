69a11ea328d3839208535b283d89011557f731ad
m.gallos <marcin.gallos@gmail.com>
2026-04-19 15:29:29 +0200

    Update fruit_drawer.gd

--- scripts/characters/fruit_drawer.gd
+++ scripts/characters/fruit_drawer.gd
@@ -1,4 +1,6 @@
-﻿extends Node2D
+extends Node2D
+## Rysuje owocowe kształty dla postaci.
+## Dodaj jako dziecko CharacterBody2D zamiast ColorRect.
 
 @export var fruit_type: String = "Strawberry"
 
@@ -20,18 +22,27 @@ func _draw():
 func _draw_strawberry():
 	var body_color = Color(0.9, 0.1, 0.15)
 	var leaf_color = Color(0.2, 0.7, 0.15)
+
 	var points = PackedVector2Array([
-		Vector2(-7, -2), Vector2(-8, -5), Vector2(-6, -8),
-		Vector2(6, -8), Vector2(8, -5), Vector2(7, -2),
-		Vector2(4, 6), Vector2(0, 8), Vector2(-4, 6),
+		Vector2(-7, -2),
+		Vector2(-8, -5),
+		Vector2(-6, -8),
+		Vector2(6, -8),
+		Vector2(8, -5),
+		Vector2(7, -2),
+		Vector2(4, 6),
+		Vector2(0, 8),
+		Vector2(-4, 6),
 	])
 	draw_colored_polygon(points, body_color)
+
 	var seed_color = Color(1.0, 0.9, 0.3)
 	draw_circle(Vector2(-3, -3), 0.8, seed_color)
 	draw_circle(Vector2(3, -3), 0.8, seed_color)
 	draw_circle(Vector2(-2, 1), 0.8, seed_color)
 	draw_circle(Vector2(2, 1), 0.8, seed_color)
 	draw_circle(Vector2(0, 4), 0.8, seed_color)
+
 	draw_colored_polygon(PackedVector2Array([
 		Vector2(-1, -8), Vector2(-5, -12), Vector2(-2, -10), Vector2(0, -9)
 	]), leaf_color)
@@ -41,54 +52,66 @@ func _draw_strawberry():
 	draw_colored_polygon(PackedVector2Array([
 		Vector2(-1, -9), Vector2(0, -13), Vector2(1, -9)
 	]), leaf_color)
+
 	_draw_eyes(Vector2(-3, -5), Vector2(3, -5))
 
 func _draw_grape():
 	var body_color = Color(0.55, 0.1, 0.7)
 	var highlight = Color(0.7, 0.3, 0.85)
 	var stem_color = Color(0.35, 0.55, 0.15)
+
 	var grape_positions = [
 		Vector2(-4, -4), Vector2(4, -4),
 		Vector2(-6, 0), Vector2(0, 0), Vector2(6, 0),
-		Vector2(-4, 4), Vector2(4, 4), Vector2(0, 7),
+		Vector2(-4, 4), Vector2(4, 4),
+		Vector2(0, 7),
 	]
 	for pos in grape_positions:
 		draw_circle(pos, 4.0, body_color)
 		draw_circle(pos + Vector2(-1, -1), 1.5, highlight)
+
 	draw_line(Vector2(0, -6), Vector2(0, -10), stem_color, 1.5)
 	draw_colored_polygon(PackedVector2Array([
 		Vector2(0, -9), Vector2(4, -12), Vector2(6, -10), Vector2(3, -8)
 	]), Color(0.3, 0.65, 0.2))
+
 	_draw_eyes(Vector2(-2, -1), Vector2(2, -1))
 
 func _draw_orange():
 	var body_color = Color(1.0, 0.6, 0.1)
 	var highlight = Color(1.0, 0.75, 0.35)
 	var leaf_color = Color(0.25, 0.6, 0.15)
+
 	draw_circle(Vector2(0, 0), 9.0, body_color)
 	draw_circle(Vector2(-3, -3), 3.5, highlight)
+
 	draw_line(Vector2(0, -8), Vector2(0, -12), Color(0.4, 0.3, 0.15), 2.0)
 	draw_colored_polygon(PackedVector2Array([
 		Vector2(0, -11), Vector2(5, -13), Vector2(4, -10), Vector2(1, -10)
 	]), leaf_color)
+
 	var line_color = Color(0.9, 0.5, 0.05, 0.3)
 	draw_line(Vector2(0, -8), Vector2(0, 8), line_color, 0.5)
 	draw_line(Vector2(-8, 0), Vector2(8, 0), line_color, 0.5)
+
 	_draw_eyes(Vector2(-3, -1), Vector2(3, -1))
 
 func _draw_pineapple():
 	var body_color = Color(0.85, 0.7, 0.15)
 	var pattern_color = Color(0.7, 0.5, 0.1)
 	var leaf_color = Color(0.2, 0.65, 0.15)
+
 	var points: PackedVector2Array = []
 	for i in range(20):
 		var angle = i * TAU / 20
 		points.append(Vector2(cos(angle) * 7, sin(angle) * 10 + 1))
 	draw_colored_polygon(points, body_color)
+
 	for y in range(-7, 10, 4):
 		draw_line(Vector2(-6, y), Vector2(6, y), pattern_color, 0.5)
 	for x in range(-5, 7, 4):
 		draw_line(Vector2(x, -7), Vector2(x, 10), pattern_color, 0.5)
+
 	var leaves = [
 		[Vector2(-2, -9), Vector2(-6, -17), Vector2(-1, -12)],
 		[Vector2(0, -10), Vector2(0, -18), Vector2(2, -12)],
@@ -98,47 +121,51 @@ func _draw_pineapple():
 	]
 	for leaf in leaves:
 		draw_colored_polygon(PackedVector2Array(leaf), leaf_color)
+
 	_draw_eyes(Vector2(-3, -2), Vector2(3, -2))
 
 <<<<<<< HEAD
 func _draw_lemon():
 	var body_color = Color(1.0, 0.95, 0.15)
-	var highlight = Color(1.0, 1.0, 0.55)
-	var tip_color = Color(0.75, 0.85, 0.2)
+	var highlight  = Color(1.0, 1.0, 0.55)
+	var tip_color  = Color(0.75, 0.85, 0.2)
+
 	var pts: PackedVector2Array = []
 	for i in range(24):
 		var a = i * TAU / 24
 		pts.append(Vector2(cos(a) * 9.0, sin(a) * 6.5))
 	draw_colored_polygon(pts, body_color)
+
 	draw_colored_polygon(PackedVector2Array([
 		Vector2(8, -1), Vector2(12, 0), Vector2(8, 1)
 	]), tip_color)
 	draw_colored_polygon(PackedVector2Array([
 		Vector2(-8, -1), Vector2(-12, 0), Vector2(-8, 1)
 	]), tip_color)
+
 	draw_circle(Vector2(-3, -2), 2.5, highlight)
 	_draw_eyes(Vector2(-3, 0), Vector2(3, 0))
 
 func _draw_watermelon():
 	var flesh_color = Color(0.9, 0.2, 0.3)
-	var rind_color = Color(0.25, 0.6, 0.2)
+	var rind_color  = Color(0.25, 0.6, 0.2)
 	var white_color = Color(0.85, 0.9, 0.8)
-	var seed_color = Color(0.1, 0.08, 0.06)
+	var seed_color  = Color(0.1, 0.08, 0.06)
+
 	draw_circle(Vector2(0, 0), 11.0, rind_color)
 	draw_circle(Vector2(0, 0), 9.5, white_color)
 	draw_circle(Vector2(0, 0), 8.5, flesh_color)
+
 	var stripe = Color(0.15, 0.5, 0.15)
 	draw_line(Vector2(-11, -3), Vector2(11, -3), stripe, 1.5)
 	draw_line(Vector2(-10, 3), Vector2(10, 3), stripe, 1.5)
+
 	for sp in [Vector2(-4, -2), Vector2(2, -3), Vector2(-2, 2), Vector2(4, 1)]:
 		draw_circle(sp, 1.2, seed_color)
+
 	_draw_eyes(Vector2(-3, -2), Vector2(3, -2))
 
 func _draw_eyes(left_pos: Vector2, right_pos: Vector2):
-<<<<<<< HEAD
-	draw_circle(left_pos, 2.0, Color.WHITE)
-	draw_circle(right_pos, 2.0, Color.WHITE)
-=======
 	draw_circle(left_pos, 2.0, Color.WHITE)
 	draw_circle(right_pos, 2.0, Color.WHITE)
 =======
@@ -148,6 +175,5 @@ func _draw_eyes(left_pos: Vector2, right_pos: Vector2):
 	draw_circle(right_pos, 2.0, Color.WHITE)
 	# Źrenice
 >>>>>>> 5382e3a9fe5f451d31955617477d07f42102f009
->>>>>>> e03f33fdfbd4476d7a3ac8fe97fbfe45a5b28e38
 	draw_circle(left_pos + Vector2(0.5, 0.5), 1.0, Color.BLACK)
 	draw_circle(right_pos + Vector2(0.5, 0.5), 1.0, Color.BLACK)
